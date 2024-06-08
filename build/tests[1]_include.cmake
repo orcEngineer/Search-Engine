fc82abb176c027b2904cad60399c527828dc0c4e
orcEngineer <orcEngineer666@gmail.com>
2024-06-08 10:42:22 +0300

    Удаление ненужных файлов и обновление .gitignore

---
+++ build/tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "D:/projects/file_search_engine/build/tests[1]_tests.cmake")
+  include("D:/projects/file_search_engine/build/tests[1]_tests.cmake")
+else()
+  add_test(tests_NOT_BUILT tests_NOT_BUILT)
+endif()
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: D:/projects/file_search_engine
+# Build directory: D:/projects/file_search_engine/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("D:/projects/file_search_engine/cmake-build-debug/file_search_engine[1]_include.cmake")
+subdirs("nlohmann.json")
+subdirs("_deps/googletest-build")
