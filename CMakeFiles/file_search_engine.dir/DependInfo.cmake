4cc0589da8437cbb8684cf021fdf1a0d6d5c79e0
orcEngineer <orcEngineer666@gmail.com>
2024-06-10 20:48:54 +0300

    обновление .gitignore

---
+++ CMakeFiles/file_search_engine.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/projects/file_search_engine/src/ConverterJSON.cpp" "CMakeFiles/file_search_engine.dir/src/ConverterJSON.cpp.obj" "gcc" "CMakeFiles/file_search_engine.dir/src/ConverterJSON.cpp.obj.d"
+  "D:/projects/file_search_engine/src/InvertedIndex.cpp" "CMakeFiles/file_search_engine.dir/src/InvertedIndex.cpp.obj" "gcc" "CMakeFiles/file_search_engine.dir/src/InvertedIndex.cpp.obj.d"
+  "D:/projects/file_search_engine/src/SearchServer.cpp" "CMakeFiles/file_search_engine.dir/src/SearchServer.cpp.obj" "gcc" "CMakeFiles/file_search_engine.dir/src/SearchServer.cpp.obj.d"
+  "D:/projects/file_search_engine/src/main.cpp" "CMakeFiles/file_search_engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/file_search_engine.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
